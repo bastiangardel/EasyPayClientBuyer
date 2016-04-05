7182fca93dac5c44293f037b6f75699d1cf07bb0
Bastian Gardel <bastiangardel@gmail.com>
2016-04-05 14:57:11 +0200

    Some Test with HTTPS request

--- Client_Buyer/ViewController.swift
+++ Client_Buyer/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+   @IBOutlet weak var text: UITextView!
+   
+   @IBOutlet weak var web: UIWebView!
+   
    override func viewDidLoad() {
       super.viewDidLoad()
       // Do any additional setup after loading the view, typically from a nib.
+      
+      self.text.editable = false;
+      
+      
+      Alamofire.request(.GET, "https://alldebrid.fr/")
+         .responseString { response in
+            print("Success: \(response.result.isSuccess)")
+            print("Response String: \(response.result.value)")
+            
+            self.text.text = response.result.value;
+            
+            let url = NSURL (string: "https://alldebrid.fr/");
+            let requestObj = NSURLRequest(URL: url!);
+            self.web.loadRequest(requestObj);
+      }
+      
+      
    }
 
    override func didReceiveMemoryWarning() {
@@ -22,4 +44,3 @@ class ViewController: UIViewController {
 
 
 }
-
