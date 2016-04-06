11f0641df1ff10689d7781774f2e4fb46309ab9c
Bastian Gardel <bastiangardel@gmail.com>
2016-04-06 20:10:51 +0200

    Update UI

--- TB_Client_Buyer/TB_Client_Buyer/ViewController.swift
+++ TB_Client_Buyer/TB_Client_Buyer/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   @IBOutlet weak var LoginB: UIButton!
+   
    override func viewDidLoad() {
       super.viewDidLoad()
       // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,11 @@ class ViewController: UIViewController {
    }
 
 
+   @IBAction func loginAction(sender: AnyObject) {
+      
+      self.performSegueWithIdentifier("loginSegue", sender: self)
+      
+      
+   }
 }
 
